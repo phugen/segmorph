39f324fc8057761a695dce219979f53b0d424516
phugen <p_huge01@uni-muenster.de>
2017-01-10 02:20:33 +0100

    - Added quick bmp -> png + resizing script - fixed mirroring - Found "magic dimensions" for U-Net

---
+++ Network/cell_quantifier/scripts/bmp_to_png.py
@@ -0,0 +1,43 @@
+# This script converts all images and their labels on the given input path
+# from BMP to PNG and saves them to the given output path.
+
+import sys
+import os
+import glob
+from PIL import Image
+
+if len(sys.argv) < 3:
+    print "Too few arguments!"
+    print "Usage: python bmp_to_png.py /path/to/input_folder /path/to/output_folder"
+    sys.exit(-1)
+
+# paths to the image folder containing images and their labels
+# and the path to the output HDF5 file
+path = sys.argv[1]
+outpath = sys.argv[2]
+
+# check how many images (minus labels) there are in the folder
+# (assuming every image in the folder has a corresponding label image!)
+all_images = glob.glob(path + "/*." + "bmp")
+IMG_NO = len(all_images)
+
+if IMG_NO == 0:
+    print "No BMP images found in path" + path + "!"
+    sys.exit(-1)
+
+print "Found " + str(IMG_NO/2) + " image(s) with label(s) in folder."
+print ""
+
+for filename in all_images:
+    if not filename.endswith("_label.bmp"): # treat label images inside the loop
+        with Image.open(filename) as bmp_img, Image.open(filename[:-4] + "_label.bmp") as bmp_label:
+
+            # resize to "magic dimensions" 596H x 948W given by data dims and U-Net conditions
+            bmp_img = bmp_img.resize((420, 420))
+            bmp_label = bmp_label.resize((420, 420))
+
+            bmp_img.save(filename[:-4] + ".png", "PNG")
+            bmp_label.save(filename[:-4] + "_label.png", "PNG")
+
+
+print "Wrote " + str(IMG_NO/2) + " PNG image(s) with label(s) to " + os.path.abspath(outpath)
--- Network/cell_quantifier/scripts/images_labels_to_hdf5.py
+++ Network/cell_quantifier/scripts/images_labels_to_hdf5.py
@@ -18,14 +18,15 @@ FORMAT = "png" # set file extension of format to look for (without dot)
 print "Format set as " + FORMAT + "."
 print ""
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     print "Too few arguments!"
-    print "Usage: python images_labels_to_hdf5.py /path/to/image_folder"
+    print "Usage: python images_labels_to_hdf5.py /path/to/input_folder /path/to/output_folder"
     sys.exit(-1)
 
-# path to the image folder containing images and their labels
-# (Passed on the command line)
+# paths to the image folder containing images and their labels
+# and the path to the output HDF5 file
 path = sys.argv[1]
+hdf5path = sys.argv[2]
 
 # check how many images (minus labels) there are in the folder
 # (assuming every image in the folder has a corresponding label image!)
@@ -34,7 +35,7 @@ IMG_NO = len(all_images)
 
 if IMG_NO == 0:
     print "No images found in path" + path + " with format ." + FORMAT + "!"
-    sys,exit(-1)
+    sys.exit(-1)
 
 if (IMG_NO % 2) != 0:
     print("Number of images didn't match number of labels!") # not foolproof - could use #labels == #images check
@@ -81,10 +82,6 @@ for filename in all_images:
 
 print ""
 
-#print "img1:\n" + str(label_array[0,:,:,:])
-#print "img2:\n" + str(data_array[1,:,:,:])
-#print "img3:\n" + str(data_array[2,:,:,:])
-
 # translate label colors to integer labels in vectorized fashion
 print "Translating colored label pixels to integers ..."
 start = timeit.default_timer()
@@ -118,30 +115,48 @@ print ""
 
 
 
-# padding parameters
+# path to network definition
 train_model_def_file = "unet-train.prototxt"
+# where to save trained parameters
 model_file = "unet.caffemodel"
-n_tiles = 1 # adjust this depending on image size / GPU mem size
+# adjust this depending on image size / GPU mem size
+n_tiles = 1
+# = applying 2x2 pooling four times
 downsampleFactor = 16
+# Size of the image when it reaches layer d4a
 d4a_size = 0
+# by how many pixels must the image be enlarged to reach original size when reaching layer d4a + 2 convs?
 padInput =   (((d4a_size * 2 + 2 + 2) * 2 + 2 + 2) * 2 + 2 + 2) * 2 + 2 + 2
+# by how many pixels will the image be shrunk when it reaches layer d4a + 2 convs?
 padOutput = ((((d4a_size - 2 - 2) * 2 - 2 - 2) * 2 - 2 - 2) * 2 - 2 - 2) * 2 - 2 - 2
+# use average of flipped tile results for classification
 average_mirror = True
 
-# calculate the image's size once it reaches layer d4a
+# calculate d4a_size (d4a_size is a misnomer - two convs follow the layer d4a, so it's layer d4c's image size!),
 # and consequently, the padding needed for the convolutions to work
 # (shape of data array: (numimages, channels, height, width))
-d4a_size = np.divide(np.subtract([data_array.shape[2], (data_array.shape[3] / n_tiles)], padOutput), downsampleFactor)
-input_size = downsampleFactor * d4a_size + padInput # [height, width] vector
-output_size = downsampleFactor * d4a_size + padOutput
+d4a_size = np.ceil(((np.array([data_array.shape[2], np.ceil(data_array.shape[3] / n_tiles)]) - padOutput) / float(downsampleFactor))) \
+             .astype(dtype=np.int)
+input_size = downsampleFactor * d4a_size + padInput # size the image needs to be padded to
+output_size = downsampleFactor * d4a_size + padOutput # original size of the image == size before classification
+border = np.around(np.subtract(input_size, output_size)) / 2; # equal padding on both sides
 
+# TODO: (d4a_size + 124) % 16 == 0, dimension condition?
+
+for i in range (0, 960):
+    if (i + 124) % 16 == 0:
+        print i
+
+print "Image dims: " + str(data_array.shape[2:4])
+print "padInput: " + str(padInput)
+print "padOutput: " + str(padOutput)
 print "d4a_size: " + str(d4a_size)
 print "input_size: " + str(input_size)
 print "output_size: " + str(output_size)
+print "border_size: " + str(border)
 
 # create zero-padded data array using
 # padding values calculated above
-border = np.around(np.subtract(input_size, output_size)) / 2; # equal padding on both sides
 paddedFullVolume = np.zeros((data_array.shape[0], \
                             data_array.shape[1], \
                             data_array.shape[2] + 2 * border[0], \
@@ -155,9 +170,10 @@ paddedFullVolume[:, \
                  border[0]:border[0] + data_array.shape[2], \
                  border[1]:border[1] + data_array.shape[3]] = data_array;
 
+print paddedFullVolume.shape
+
 
 # fill zero-padded areas with meaningful data
-# (in this case: mirror the available image data at the borders)
 
 xpad  = border[0] - 1 # last padding element before image data
 xfrom = border[0] # first image element
@@ -167,26 +183,23 @@ ypad  = border[1] - 1
 yfrom = border[1]
 yto   = border[1] + data_array.shape[2]
 
-
-# fill areas left, right, above and below the actual image data
-#paddedFullVolume[:, :, 0:yfrom, 0:xfrom] = paddedFullVolume[:, :, yfrom:yfrom+ypad+1, xfrom + xpad:xfrom-1:-1] # use negative step for "reverse" slicing
-#paddedFullVolume[:, :, 0:yfrom, xto-1:xto + xpad] = paddedFullVolume[:, :, yfrom:yfrom+ypad+1, xto:xto + 1 - xpad:-1]
-
-# use negative step for "reverse" slicing -> mirroring at axis
-paddedFullVolume[:, :, yfrom:yto, 0:xfrom] = paddedFullVolume[:, :, yfrom:yto, xfrom + xpad + 1:xfrom:-1] #left
-paddedFullVolume[:, :, yfrom:yto, xto:xto + xpad + 1] = paddedFullVolume[:, :, yfrom:yto, xto - 1:xto - xpad - 2:-1] #right
-paddedFullVolume[:, :, 0:yfrom, xfrom:xto] = paddedFullVolume[:, :, yfrom + ypad + 1:yfrom:-1, xfrom:xto] # above
+# fill all 8 zero-padded rectangles adjacent to image with mirrored image data
+paddedFullVolume[:, :, yfrom:yto, 0:xfrom] = paddedFullVolume[:, :, yfrom:yto, xfrom + xpad:xfrom - 1:-1] # left
+paddedFullVolume[:, :, yfrom:yto, xto:xto + xpad + 1] = paddedFullVolume[:, :, yfrom:yto, xto - 1:xto - xpad - 2:-1] # right
+paddedFullVolume[:, :, 0:yfrom, xfrom:xto] = paddedFullVolume[:, :, yfrom + ypad:yfrom - 1:-1, xfrom:xto] # above
 paddedFullVolume[:, :, yto:yto + ypad + 1, xfrom:xto] = paddedFullVolume[:, :, yto - 1:yto - ypad - 2:-1, xfrom:xto] # below
 
-paddedFullVolume[:, :, 0:yfrom, 0:xfrom] = paddedFullVolume[:, :, yfrom + ypad + 1:yfrom:-1, xfrom + xpad + 1:xfrom:-1] # top left
-paddedFullVolume[:, :, 0:yfrom, xto:xto + xpad + 1] = paddedFullVolume[:, :, yfrom + ypad + 1:yfrom:-1, xto - 1:xto - xpad - 2:-1] # top right
-paddedFullVolume[:, :, yto:yto + ypad + 1, 0:xfrom] = paddedFullVolume[:, :, yto:yto - ypad - 1:-1, xfrom + xpad + 1:xfrom:-1] # bottom left <-- one pixel too low?
-paddedFullVolume[:, :, yto:yto + ypad + 1, xto:xto + xpad + 1] = paddedFullVolume[:, :, yto:yto - ypad - 1:-1, xto - 1:xto - xpad - 2:-1] # bottom right
+paddedFullVolume[:, :, 0:yfrom, 0:xfrom] = paddedFullVolume[:, :, yfrom + ypad:yfrom - 1:-1, xfrom + xpad:xfrom - 1:-1] # top left
+paddedFullVolume[:, :, 0:yfrom, xto:xto + xpad + 1] = paddedFullVolume[:, :, yfrom + ypad:yfrom - 1:-1, xto - 1:xto - xpad - 2:-1] # top right
+paddedFullVolume[:, :, yto:yto + ypad + 1, 0:xfrom] = paddedFullVolume[:, :, yto - 1:yto - ypad - 2:-1, xfrom + xpad:xfrom - 1:-1] # bottom left
+paddedFullVolume[:, :, yto:yto + ypad + 1, xto:xto + xpad + 1] = paddedFullVolume[:, :, yto - 1:yto - ypad - 2:-1, xto - 1:xto - xpad - 2:-1] # bottom right
 
 
 start = timeit.default_timer()
+
 # assign data to according HDF dataset and write HDF file to HDD
-with h5py.File("../training/drosophila_training.h5", "w", libver="latest") as f:
+print "Writing HDF5 file to " + hdf5path + " ..."
+with h5py.File(hdf5path, "w", libver="latest") as f:
 
     f.create_dataset("data", dtype=np.float32, data=paddedFullVolume)
     f.create_dataset("label", dtype=np.uint8, data=label_array_int)
@@ -194,16 +207,27 @@ with h5py.File("../training/drosophila_training.h5", "w", libver="latest") as f:
     f.attrs["CLASS"] = "IMAGE"
     f.attrs["IMAGE_VERSION"] = "1.2"
     f.attrs["IMAGE_SUBCLASS"] =  "IMAGE_TRUECOLOR"
-    #f.attrs["IMAGE_MINMAXRANGE"] = np.array([0,1], dtype=np.float32)
-    #f.attrs["PALETTE"] =
 
-    #f.close()
 
 print "Writing to HDF5 took " + str(timeit.default_timer() - start) + " ms."
+
+# Write test network definition to file
+print "Writing test network definition to ../unet-test.prototxt ..."
+with open("../unet.prototxt", "r") as net, open("../unet-test.prototxt", "w") as test_net:
+    test_net.write("input: \"data\"\n"); # batch size
+    test_net.write("input_dim: " + str(data_array.shape[1]) + "\n"); # channels
+    test_net.write("input_dim: " + str(input_size[0]) + "\n"); # height
+    test_net.write("input_dim: " + str(input_size[1]) + "\n"); # width
+    test_net.write("state: { phase: TEST }" + "\n");
+    test_net.write(net.read()); # the rest of the network definition
+
+
 print "Done!"
 
-with h5py.File("../training/drosophila_training.h5", "r", libver="latest") as f:
-    print f["data"].shape
-    print f["label"].shape
+#with h5py.File("../training/drosophila_training.h5", "r", libver="latest") as f:
+#    print f["data"].shape
+#    print f["label"].shape
+
+
 
 # TODO: REMEMBER: even predictions need dummy labels.. apparently. See segmentAndTrack2.m
